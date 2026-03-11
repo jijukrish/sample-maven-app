9c48f7bcdbd3d5e9c14a17b338536d84ae1e5bd9
jijukrish <jijukrishnan@hotmail.com>
2026-03-11 13:45:00 +1100

    Add check for MY_API_KEY1 environment variable

--- test.java
+++ test.java
@@ -4,4 +4,9 @@ String apiKey = System.getenv("MY_API_KEY");
       System.err.println("MY_API_KEY environment variable not set!");
       // Handle error appropriately
   }
+  String apiKey1 = System.getenv("MY_API_KEY1");
+  if (apiKey1 == null) {
+      System.err.println("MY_API_KEY1 environment variable not set!");
+      // Handle error appropriately
+  }
 }
