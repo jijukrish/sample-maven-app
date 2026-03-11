f3c0602e095adf854986f2f2b0bc4772c3521caf
jijukrish <jijukrishnan@hotmail.com>
2026-03-11 14:35:15 +1100

    Modify apiKey value in test.java
    Updated apiKey to include additional characters.

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public static void main(){
   String MY_API_KEY="ferett565656565wewrwer"
-String apiKey = "ghp_A1b2C3d4E5f6G7h8I9j0K1l2M3n4O5p6Q7r8S9t045656757fhgjoowewo"
+String apiKey = "ghp_A1b2C3d4E5f6G7h8I9j0K1l2M3n4O5p6Q7r8S9t045656757fhgjoowewo123"
   if (apiKey == null) {
       System.err.println("MY_API_KEY environment variable not set!");
       // Handle error appropriately
