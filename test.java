3f42403ebc3d73fc0dc1b26f5afac75371c6c666
jijukrish <jijukrishnan@hotmail.com>
2026-03-11 13:56:59 +1100

    Update API keys in test.java

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 public static void main(){
-  String MY_API_KEY="ferett565656565"
+  String MY_API_KEY="ferett565656565wewrwer"
 String apiKey = "45656757fhgjoowewo"
   if (apiKey == null) {
       System.err.println("MY_API_KEY environment variable not set!");
       // Handle error appropriately
   }
-  String apiKey1 = "45656757fhgjoowewo"
+  String apiKey1 = "45656757fhgjoowewotest"
   if (apiKey1 == null) {
       System.err.println("MY_API_KEY1 environment variable not set!");
       // Handle error appropriately
