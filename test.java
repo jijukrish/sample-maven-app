31e130b30fe7ddcb4a54945dbcacd978a18ac85c
jijukrish <jijukrishnan@hotmail.com>
2026-03-11 13:54:57 +1100

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,11 @@
 public static void main(){
-String apiKey = System.getenv("MY_API_KEY");
+  String MY_API_KEY="ferett565656565"
+String apiKey = "45656757fhgjoowewo"
   if (apiKey == null) {
       System.err.println("MY_API_KEY environment variable not set!");
       // Handle error appropriately
   }
-  String apiKey1 = System.getenv("MY_API_KEY1");
+  String apiKey1 = "45656757fhgjoowewo"
   if (apiKey1 == null) {
       System.err.println("MY_API_KEY1 environment variable not set!");
       // Handle error appropriately
